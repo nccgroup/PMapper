05ad300fa56626fa5489140cd43adf933c304dbe
Erik Steringer <Erik.Steringer@nccgroup.trust>
2019-08-21 21:20:34 -0700

    set up SSM edge checking

--- principalmapper/graphing/ssm_edges.py
+++ principalmapper/graphing/ssm_edges.py
@@ -26,4 +26,44 @@ class SSMEdgeChecker(EdgeChecker):
                 if node_source.is_admin:
                     continue
 
+                # check if destination is a role with an instance profile
+                if ':role/' not in node_destination.arn or node_destination.instance_profile is None:
+                    continue
+
+                # at this point, we make an assumption that some instance is operating with the given instance profile
+                # we assume if the role can call ssmmessages:CreateControlChannel, anyone with ssm perms can access it
+                if not query_interface.local_check_authorization(node_destination, 'ssmmessages:CreateControlChannel',
+                                                                 '*', {}, False):
+                    continue
+
+                # so if source can call ssm:SendCommand or ssm:StartSession, it's an edge
+                cmd_auth_res, mfa_res_1 = query_interface.local_check_authorization_handling_mfa(
+                    node_source,
+                    'ssm:SendCommand',
+                    '*',
+                    {},
+                    False
+                )
+
+                if cmd_auth_res:
+                    reason = 'can call ssm:SendCommand to access an EC2 instance with access to'
+                    if mfa_res_1:
+                        reason = '(Requires MFA) ' + reason
+                    result.append(Edge(node_source, node_destination, reason))
+
+
+                sesh_auth_res, mfa_res_2 = query_interface.local_check_authorization_handling_mfa(
+                    node_source,
+                    'ssm:StartSession',
+                    '*',
+                    {},
+                    False
+                )
+
+                if sesh_auth_res:
+                    reason = 'can call ssm:StartSession to access an EC2 instance with access to'
+                    if mfa_res_2:
+                        reason = '(Requires MFA) ' + reason
+                    result.append(Edge(node_source, node_destination, reason))
+
         return result
