25dd7a2bf5cd5f2f67f6603897b05531e80630d4
Erik Steringer <Erik.Steringer@nccgroup.trust>
2019-08-22 10:23:16 -0700

    completed EC2 edge checking

--- principalmapper/graphing/ec2_edges.py
+++ principalmapper/graphing/ec2_edges.py
@@ -48,8 +48,9 @@ class EC2EdgeChecker(EdgeChecker):
                     continue  # EC2 wasn't auth'd to assume the role
 
                 # check if source can pass the destination role
+                mfa_needed = False
                 condition_keys = {'iam:PassedToService': 'ec2.amazonaws.com'}
-                pass_role_auth, need_mfa_to_pass = query_interface.local_check_authorization_handling_mfa(
+                pass_role_auth, mfa_res = query_interface.local_check_authorization_handling_mfa(
                     node_source,
                     'iam:PassRole',
                     node_destination.arn,
@@ -60,31 +61,48 @@ class EC2EdgeChecker(EdgeChecker):
                     continue  # source can't pass the role to use it
 
                 # check if destination has an instance profile, if not: check if source can create it
-                need_mfa_for_ip = False
                 if node_destination.instance_profile is None:
-                    create_ip_auth, need_mfa_0 = query_interface.local_check_authorization_handling_mfa(
+                    create_ip_auth, mfa_res = query_interface.local_check_authorization_handling_mfa(
                         node_source, 'iam:CreateInstanceProfile', '*', {}, debug)
                     if not create_ip_auth:
                         continue  # node_source can't make the instance profile
-                    if need_mfa_0:
-                        need_mfa_for_ip = True
+                    if mfa_res:
+                        mfa_needed = True
 
-                    create_ip_auth, need_mfa_0 = query_interface.local_check_authorization_handling_mfa(
+                    create_ip_auth, mfa_res = query_interface.local_check_authorization_handling_mfa(
                         node_source, 'iam:AddRoleToInstanceProfile', node_destination.arn, {}, debug)
                     if not create_ip_auth:
                         continue  # node_source can't attach a new instance profile to node_destination
-                    if need_mfa_0:
-                        need_mfa_for_ip = True
+                    if mfa_res:
+                        mfa_needed = True
 
                 # check if source can run an instance with the instance profile condition, add edge if so and continue
-                iprofile = node_destination.instance_profile if node_destination.instance_profile is not None else '*'
-                condition_keys = {'ec2:InstanceProfile': iprofile}
-                if query_interface.local_check_authorization(node_source, 'ec2:RunInstances', '*', condition_keys,
-                                                             debug):
-                    if iprofile is not None:
+                if node_destination.instance_profile is not None:
+                    iprofile = node_destination.instance_profile
+                    condition_keys = {'ec2:InstanceProfile': iprofile}
+                else:
+                    iprofile = '*'
+                    condition_keys = {}
+
+                create_instance_res, mfa_res = query_interface.local_check_authorization_handling_mfa(
+                    node_source,
+                    'ec2:RunInstances',
+                    '*',
+                    condition_keys,
+                    debug
+                )
+
+                if mfa_res:
+                    mfa_needed = True
+
+                if create_instance_res:
+                    if iprofile is not '*':
                         reason = 'can use EC2 to run an instance with an existing instance profile to access'
                     else:
                         reason = 'can use EC2 to run an instance with a newly created instance profile to access'
+                    if mfa_needed:
+                        reason = '(MFA required) ' + reason
+
                     new_edge = Edge(
                         node_source,
                         node_destination,
@@ -94,18 +112,43 @@ class EC2EdgeChecker(EdgeChecker):
                     result.append(new_edge)
 
                 # check if source can run an instance without an instance profile then add the profile, add edge if so
-                if query_interface.local_check_authorization(node_source, 'ec2:RunInstances', '*', {}, debug):
-                    if iprofile is not None:
-                        reason = 'can use EC2 to run an instance and then attach an existing instance profile to access'
+                create_instance_res, mfa_res = query_interface.local_check_authorization_handling_mfa(
+                    node_source,
+                    'ec2:RunInstances',
+                    '*',
+                    {},
+                    debug
+                )
+
+                if mfa_res:
+                    mfa_needed = True
+
+                if create_instance_res:
+                    attach_ip_res, mfa_res = query_interface.local_check_authorization_handling_mfa(
+                        node_source,
+                        'ec2:AssociateIamInstanceProfile',
+                        '*',
+                        condition_keys,
+                        debug
+                    )
+
+                    if iprofile is not '*':
+                        reason = 'can use EC2 to run an instance and then associate an existing instance profile to ' \
+                                 'access'
                     else:
                         reason = 'can use EC2 to run an instance and then attach a newly created instance profile to ' \
                                  'access'
-                    new_edge = Edge(
-                        node_source,
-                        node_destination,
-                        reason
-                    )
-                    output.write('Found new edge: {}\n'.format(new_edge.describe_edge()))
-                    result.append(new_edge)
+
+                    if mfa_res or mfa_needed:
+                        reason = '(MFA required) ' + reason
+
+                    if attach_ip_res:
+                        new_edge = Edge(
+                            node_source,
+                            node_destination,
+                            reason
+                        )
+                        output.write('Found new edge: {}\n'.format(new_edge.describe_edge()))
+                        result.append(new_edge)
 
         return result
