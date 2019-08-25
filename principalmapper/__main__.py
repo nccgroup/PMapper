319503462b99c4be2e864d93f9ea38f627e3d296
Erik Steringer <Erik.Steringer@nccgroup.trust>
2019-08-24 21:41:24 -0700

    Final correction of license notifications

--- principalmapper/__main__.py
+++ principalmapper/__main__.py
@@ -2,7 +2,7 @@
 Provides a command-line interface to use the principalmapper library
 """
 
-#  Copyright NCC Group (c) 2019. This file is part of Principal Mapper.
+#  Copyright (c) NCC Group and Erik Steringer 2019. This file is part of Principal Mapper.
 #
 #      Principal Mapper is free software: you can redistribute it and/or modify
 #      it under the terms of the GNU Affero General Public License as published by
@@ -16,19 +16,6 @@ Provides a command-line interface to use the principalmapper library
 #
 #      You should have received a copy of the GNU Affero General Public License
 #      along with Principal Mapper.  If not, see <https://www.gnu.org/licenses/>.
-#
-#      Principal Mapper is free software: you can redistribute it and/or modify
-#      it under the terms of the GNU Affero General Public License as published by
-#      the Free Software Foundation, either version 3 of the License, or
-#      (at your option) any later version.
-#
-#      Principal Mapper is distributed in the hope that it will be useful,
-#      but WITHOUT ANY WARRANTY; without even the implied warranty of
-#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#      GNU General Public License for more details.
-#
-#      You should have received a copy of the GNU Affero General Public License
-#      along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
 
 import argparse
 import os
--- tests/test_edge_identification.py
+++ tests/test_edge_identification.py
@@ -1,6 +1,6 @@
 """Test code for the edge-identifying functions and classes"""
 
-#  Copyright NCC Group (c) 2019. This file is part of Principal Mapper.
+#  Copyright (c) NCC Group and Erik Steringer 2019. This file is part of Principal Mapper.
 #
 #      Principal Mapper is free software: you can redistribute it and/or modify
 #      it under the terms of the GNU Affero General Public License as published by
@@ -14,19 +14,6 @@
 #
 #      You should have received a copy of the GNU Affero General Public License
 #      along with Principal Mapper.  If not, see <https://www.gnu.org/licenses/>.
-#
-#      Principal Mapper is free software: you can redistribute it and/or modify
-#      it under the terms of the GNU Affero General Public License as published by
-#      the Free Software Foundation, either version 3 of the License, or
-#      (at your option) any later version.
-#
-#      Principal Mapper is distributed in the hope that it will be useful,
-#      but WITHOUT ANY WARRANTY; without even the implied warranty of
-#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#      GNU General Public License for more details.
-#
-#      You should have received a copy of the GNU Affero General Public License
-#      along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
 
 import unittest
 
